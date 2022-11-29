68c506d57530b6d5d1f82f83078f96424ee46211
GLuca27 <gianluca.sciglitano1@gmail.com>
2022-11-29 16:21:10 +0100

    aaa

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const images = [
 const items = document.querySelector(".items")
 
 //ciclo sull'array delle immagini
-for ( let i = 0; i < images.length; i++ ){
+for( let i = 0; i < images.length; i++ ){
     const srcImg = images[i];
     //creo l'elemento
     const item = document.createElement("div");
@@ -19,9 +19,40 @@ for ( let i = 0; i < images.length; i++ ){
      if ( i === 0 ){
         item.classList.add("active")
      }
-     item.innerHTML = `<img src"${srcImg}" alt"">`;
+     item.innerHTML = `
+          <img src="${srcImg}" alt="">
+     `;
 
      items.append(item);
 }
+
+document.querySelector(".item").classList.add("active");
+
+let currentImageIndex = 0;
+
+// seleziono e aggiungo evento click a next (1o metodo che vale anche per prev)
+const nextBtn = document.querySelector(".next");
+ nextBtn.addEventListener("click", function(){
+   items[currentImageIndex].classList.remove("active");
+    if( currentImageIndex === 0 ){
+       currentImageIndex = images.length - 1;
+    } else {
+       currentImageIndex++;
+    }
+   items[currentImageIndex].classList.add("active");
+ })
+
+ //seleziono e aggiungo l'evento click al pulsante prev (2o metodo che vale anche per next)
+ 
+const prevBtn = document.querySelector(".prev");
+ prevBtn.addEventListener("click", function(){
+   items[currentImageIndex].classList.remove("active");
+    if( currentImageIndex === 0 ){
+       currentImageIndex = images.length - 1;
+    } else {
+       currentImageIndex--;
+    }
+   items[currentImageIndex].classList.add("active");
+ })
 
 
